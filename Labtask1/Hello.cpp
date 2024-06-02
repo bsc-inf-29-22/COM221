8794553e4a78b9203f578407542e22e06181a627
BSC-INF-29-22 <bsc-inf-29-22@unima.ac.mw>
2024-06-02 19:47:28 +0100

    i have just organized labtasks

---
+++ Labtask1/Hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // main() is where program execution begins.
+    cout<<"Hello world\n\n\n"<<"New line skipping"<<endl<<endl<<"We are introduced to c++ programming"; //Prints Hello world to the console.
+return 0;
+}
